40b01022d0e8a5195976a9c72fe4874c55bcb8dd
RustyMew <62964078+RustyMew@users.noreply.github.com>
2020-04-11 14:40:53 -0400

    Initial commit

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1586630170,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/platform-sheet0.png",
+		"images/runner-sheet0.png",
+		"images/wall-sheet0.png",
+		"images/deathparticles.png",
+		"images/winplatform-sheet0.png",
+		"media/countdowna.ogg",
+		"media/countdownb.ogg",
+		"media/jump.m4a",
+		"media/jump.ogg",
+		"media/death.m4a",
+		"media/death.ogg",
+		"media/intro.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
